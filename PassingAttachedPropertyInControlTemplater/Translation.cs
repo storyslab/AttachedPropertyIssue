c776b2d6ed1a7f15d7dc7c0a80fc1f1f48ea6830
unknown <jeffhaines@me.com>
2019-06-17 13:13:06 -0400

    init example

---
+++ PassingAttachedPropertyInControlTemplater/Translation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace PassingAttachedPropertyInControlTemplater
+{
+    class Translation
+    {
+        public static int GetTranslationModelID(DependencyObject obj)
+        {
+            return (int)obj.GetValue(TranslationModelIDProperty);
+        }
+
+        public static void SetTranslationModelID(DependencyObject obj, int value)
+        {
+            obj.SetValue(TranslationModelIDProperty, value);
+        }
+
+        public static readonly DependencyProperty TranslationModelIDProperty =
+            DependencyProperty.RegisterAttached("TranslationModelID", typeof(int), typeof(FrameworkElement), new PropertyMetadata(0));
+
+    }
+}
